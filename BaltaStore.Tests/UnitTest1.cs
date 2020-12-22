293679aefec2832452962ab3ad4818931eb94474
Antonio Sucre <ajs2104@gmail.com>
2020-12-22 18:58:55 -0300

    Adjusting Entities

--- BaltaStore.Tests/UnitTest1.cs
+++ BaltaStore.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using BaltaStore.Domain.StoreContext.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BaltaStore.Tests
@@ -8,6 +9,11 @@ namespace BaltaStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            var customer = new Customer("Antonio", "Sucre", "000000000", "ajs21@gmail.com", "0000", "Rua 3712");
+            var order = new Order(customer);
+
+            // order.Items.Add();
+
         }
     }
 }
