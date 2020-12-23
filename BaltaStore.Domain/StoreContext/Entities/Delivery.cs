13e4af7f5f810d8584b73e97221265a3b48133b7
Antonio Sucre <ajs2104@gmail.com>
2020-12-22 22:44:17 -0300

    Modeling Order

--- BaltaStore.Domain/StoreContext/Entities/Delivery.cs
+++ BaltaStore.Domain/StoreContext/Entities/Delivery.cs
@@ -5,13 +5,26 @@ namespace BaltaStore.Domain.StoreContext.Entities
 {
     public class Delivery
     {
-        public Delivery(DateTime estimatedDeliveryDate){
+        public Delivery(DateTime estimatedDeliveryDate)
+        {
             CreateDate = DateTime.Now;
             EstimatedDeliveryDate = estimatedDeliveryDate;
             Status = EDeliveryStatus.Waiting;
-        }       
+        }
         public DateTime CreateDate { get; private set; }
         public DateTime EstimatedDeliveryDate { get; private set; }
         public EDeliveryStatus Status { get; private set; }
+
+        public void Ship()
+        {
+            //Se a data estimada de entrega for no passado, não entregar
+            Status = EDeliveryStatus.Shipped;
+        }
+
+        public void Cancel()
+        {
+            //Se o status já for entregue não pode cancelar
+            Status = EDeliveryStatus.Canceled;
+        }
     }
 }
