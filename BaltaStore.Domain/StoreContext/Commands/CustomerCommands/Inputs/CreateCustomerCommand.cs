8cc50973736f7849b9cc1ca3073a010561291827
Antonio Sucre <ajs2104@gmail.com>
2020-12-23 16:11:41 -0300

    Adding Commands

---
+++ BaltaStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
@@ -0,0 +1,12 @@
+namespace BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Inputs
+{
+    public class CreateCustomerCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
