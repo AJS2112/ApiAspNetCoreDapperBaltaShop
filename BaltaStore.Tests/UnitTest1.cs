d4a55479ea2764fbfa12929c1693ea1dba960764
Antonio Sucre <ajs2104@gmail.com>
2020-12-22 22:10:14 -0300

    Adding ValueObjects

--- BaltaStore.Tests/UnitTest1.cs
+++ BaltaStore.Tests/UnitTest1.cs
@@ -9,8 +9,8 @@ namespace BaltaStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var customer = new Customer("Antonio", "Sucre", "000000000", "ajs21@gmail.com", "0000", "Rua 3712");
-            var order = new Order(customer);
+            //var customer = new Customer("Antonio", "Sucre", "000000000", "ajs21@gmail.com", "0000", "Rua 3712");
+            //var order = new Order(customer);
 
             // order.Items.Add();
 
