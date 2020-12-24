b14a278c6d842910c95f9e67cd543c032b55fd15
Antonio Sucre <ajs2104@gmail.com>
2020-12-23 21:25:01 -0300

    Creating DataContext

---
+++ BaltaStore.Shared/Settings.cs
@@ -0,0 +1,7 @@
+namespace BaltaShop.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = "Data source=localhost; Initial Catalog=BaltaStore; Persist Security Info=True; Integrated Security=SSPI;";
+    }
+}
